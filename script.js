6bd171300c804dda991b15477c8f2e99aa5f0059
llcain <llcain2012@gmail.com>
2024-10-08 19:15:50 -0500

    Update Json file

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
 
-// const para = document.getElementById('demo');
+import data from './assets/data/data.json';
+const frame = document.getElementById("loop");
+data.map((element) => {
+    console.log(element);
+    let newDiv = document.createElement("div");
+    newDiv.id = element.id;
+    newDiv.className = "card";
+    newDiv.innerHTML = `
+        <h2>${element.name}</h2>
+        <p>${element.desc}</p>
+        <button class="btn'>let see</button>`;
+    frame.appendChild(newDiv);
+});
 
 
-
-
-// fetch("./data/users.json")
-// .then(Response => Response.json())
-// .then(data =>  console.log(data))
+fetch("./assets/data/data.json")
+.then((response) => response.json())
+.then((data) => {
+    console.log(data);
+})
+.catch ((error) => console.log('Error loading JSON', error));
